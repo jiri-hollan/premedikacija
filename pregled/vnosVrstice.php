d785d8f3517b8d946b87449d1a6be7a85360b48b
Jiří Hollan <jiri.hollan@gmail.com>
2019-12-23 22:31:57 +0100

    Shanjevanje bolnika
    urejena koda

--- pregled/vnosVrstice.php
+++ pregled/vnosVrstice.php
@@ -12,48 +12,11 @@ $ime = $priimek = $datRojstva  = $stevMaticna = $EMSO = "";
 $imeTable = 'novBolnikTab';
 
 
-$bolnik = $_POST;
-$c=$d=$f="";
+$polja = implode(',', array_keys($_POST));
 
-foreach($bolnik as $x => $x_value) {
-	
-	    if ($x == 'Submit') {
-        break;    /* You could also write 'break 1;' here. */
-		
-    }
-		
-    /*echo "Key=" . $x . ", Value=" . $x_value;
-    echo "<br>";*/
-	$a= "$" . $x;
-	//echo $a;
-	
-	//echo "$" . $x ;
-  //  echo "$" . $x . "="  . '"' . $x_value . '"';
-   // echo "<br>";
-     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		 
-        $b  ="'" . test_input($x_value) . "'";		
-		$c = $c . ", " . $b;
-		$d = $d . ", " . $x;
-		$f = $f . ", "  . "$" . $x;
-		//echo $c;
-		//echo "<br>";
-		//echo $d; 
-		//echo "<br>";				
-}
-		
-}
-     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-$c = ltrim($c,", ");
-$polja = ltrim($d,", ");
-$f = ltrim($f,", ");
-	//echo "$c" . $c;	
-	//echo "<br>";
-	//echo $polja; 
-	//echo "<br>";
-	//echo $f;
- vstavi($imeTable,$c,$polja );
-} 
+$vrednosti = '"' . implode('","', $_POST) . '"';
+
+vstavi($imeTable, $vrednosti, $polja);
 
 function test_input($data) {
   $data = trim($data);
@@ -69,17 +32,14 @@ $username = "root";
 $password = "";
 $dbname = "premedikacija";
 
-
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO". " " . $imeTable . " " . " ($polja) VALUES ($c)";
+    $sql = "INSERT INTO " . $imeTable . " ($polja) VALUES ($c)";
     // (ime, priimek, datRojstva, stevMaticna, EMSO)
 	//echo $_SERVER["REQUEST_METHOD"];
-                                                                 
 
-	
     // use exec() because no results are returned
     $conn->exec($sql);
     echo "New record created successfully";
