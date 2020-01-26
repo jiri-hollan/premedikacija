de8c1bdb83ca3620f90751f68c6e16955e03772c
Jiří Hollan <jiri.hollan@gmail.com>
2020-01-26 11:27:51 +0100

    Popravljeni bugi

--- pregled/js/report.js
+++ pregled/js/report.js
@@ -6,7 +6,8 @@
      document.getElementById("druga").style.display = "none";
      document.getElementById("tretja").style.visibility = "visible";
 
-
+//preiskava = document.​forms["frm1"].fieldset["lab"]. ​getElementsByTagName("input")[0].name;
+//alert(preiskava);
 nalepka = document.getElementById("priimek").value;
 nalepka = "priimek in ime:.... " + "<b>" + nalepka + " " + document.getElementById("ime").value + "</b>" + "<br>";
 nalepka = nalepka + "datum rojstva:..... " +  "<b>" + document.getElementById("datRojstva").value + "</b>" + "<br>" ;
@@ -35,7 +36,7 @@ return x;
   return x;
      }
   }	
-//................................................................................................................
+//....................................Meritve..............................................
  x = document.getElementById("starost").value;
  x = xFunction(x);
 //alert(typeof x + x);
@@ -59,10 +60,44 @@ meritve =  meritve +  " " + " krvni tlak:" + "&nbsp" + "<b>" + x + "&nbsp" + "mm
 x = document.getElementById("pulz").value;
 x = xFunction(x);
 meritve =  meritve +  " " + " pulz:" + "&nbsp" + "<b>" + x + "&nbsp" + "/min " + "</b>";
-
-//alert(typeof meritve + meritve);
 document.getElementById("meritveR").innerHTML=meritve;
 
+//........laboratorij...................................................................................
+var text = "";
+var i;
+var lab = document.getElementById("lab").getElementsByTagName("label");
+var vred =document.getElementById("lab").getElementsByTagName("input"); 
+for (i = 0; i < lab.length; i++) {
+
+  text += lab[i].innerHTML + vred[i].value + "," + "&nbsp" + " ";
+  
+  //alert(text)
+}
+document.getElementById("labR").innerHTML = text;
+//....................EKG....................................................................
+
+var ekg = document.getElementById("ekg").value;
+document.getElementById("ekgR").innerHTML= "EKG: " + ekg;
+//...........................RTG................................................................
+var rtg = document.getElementById("rtg").value;
+document.getElementById("rtgR").innerHTML= "RTG: " + rtg;
+//..............pridružene bolezno........................................................
+
+var prid = document.getElementById("dgPridruzene").value;
+//alert(prid);
+document.getElementById("pridBolezniR").innerHTML= "Pridružene bolezni: " + prid;
+//................................... predhodna terapija.........................................
+var pred = document.getElementById("terPredhodna").value;
+
+document.getElementById("predTerapR").innerHTML= "Predhodna terapija: " + pred;
+//............asa mallampati, alergija.........................................................
+
+//..................Izvidi in opombe...........................................................
+var izvidi = document.getElementById("izvidiInOpombe").value;
+document.getElementById("izvidiR").innerHTML= "Izvidi in opombe: " + izvidi;
+
+//preiskava = document.​forms["frm1"].fieldset["lab"]. ​getElementsByTagName("input")[0].name;
+//alert(preiskava);
 //spa=document.getElementById("sp").innerHTML;
 //alert(typeof spa + spa);
 //document.getElementById("diagnozaR").innerHTML=spa;
