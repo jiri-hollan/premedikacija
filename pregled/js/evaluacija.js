26fa9b0a552ed6da9af6b16518b0ffdc734adbb9
Jiří Hollan <jiri.hollan@gmail.com>
2020-05-21 10:35:18 +0200

    dodani lab

--- pregled/js/evaluacija.js
+++ pregled/js/evaluacija.js
@@ -6,6 +6,13 @@ var aptc = {min:28, max:40};
 var trombociti = {min:150, max:410};
 var kreatinin = {min:72, max:127};
 
+var laktat = {min:0, max:1.8};
+var pbnp = {min:0, max:100};
+var pct = {min:0, max:0.05};
+var crp = {min:0, max:8};
+var na = {min:136, max:145};
+var k = {min:3.8, max:5};
+
  function laborFunction(ime,vrednost)
 {
   min = window[ime]["min"];
@@ -13,8 +20,7 @@ var kreatinin = {min:72, max:127};
     alert (ime + " je pod spodnjo mejo mormale");
   }
 
-max = window[ime]["max"];
-
+  max = window[ime]["max"];
   if(vrednost != "" && vrednost>max){
    alert (ime + " je nad zgornjo mejo mormale");
   }
